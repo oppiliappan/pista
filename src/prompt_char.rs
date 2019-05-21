bebbb8b0a27bf5335499d3388046685ebd602e55
NerdyPepper <akshayoppiliappan@gmail.com>
2019-05-21 23:35:38 +0530

    hotfix: default color for $ was missing

--- src/prompt_char.rs
+++ src/prompt_char.rs
@@ -4,7 +4,7 @@ use colored::*;
 pub fn get_char() -> colored::ColoredString {
     let user_char = env::var("PROMPT_CHAR").unwrap_or("$".into());
     let root_char = env::var("PROMPT_CHAR_ROOT").unwrap_or("#".into());
-    let user_char_color = env::var("PROMPT_CHAR_COLOR").unwrap_or("green ".into());
+    let user_char_color = env::var("PROMPT_CHAR_COLOR").unwrap_or("green".into());
     let root_char_color = env::var("PROMPT_CHAR_ROOT_COLOR").unwrap_or("red".into());
 
     let euid = unsafe { libc::geteuid() };
