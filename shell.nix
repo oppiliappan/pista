fed2691d6045844214fa19b2dbdfe37d299bc90a
Akshay <nerdy@peppe.rs>
2020-08-18 14:53:55 +0530

    add nix env

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+    name = "rust-env";
+    nativeBuildInputs = with pkgs; [
+        rustc cargo
+    ];
+    buildInputs = with pkgs; [ openssl pkgconfig ];
+
+    RUST_BACKTRACE = 1;
+}
